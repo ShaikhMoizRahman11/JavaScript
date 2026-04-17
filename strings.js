44d144db70f1a4ba553024f154d8eeeb9b60866f
ShaikhMoizRahman11 <shaikhmoizrahman@gmail.com>
2026-04-17 20:57:31 +0530

    added arrow functions, functions with parameters function with multiple parameters, rest parameters and Strings and methods of strings

---
+++ strings.js
@@ -0,0 +1,58 @@
+let a="Welcome";
+let b="Not Welcome";
+
+let d= a+" "+b;
+
+
+// Template litrals
+let c=`${a} freind ${b}`;
+
+// console.log(c);
+// console.log(d);
+
+
+
+
+// console.log(msg.toLowerCase());
+// console.log(msg.toUpperCase());
+
+
+//  let len=msg.length;
+// console.log(msg.includes('f'));
+// console.log(msg.startsWith('e'));
+// console.log(msg.endsWith('d'));
+
+//  console.log(msg.replace("world","galaxy"));
+
+// console.log(len);
+// console.log(msg.length);
+
+let msg='Hello world';
+// console.log(msg[1]);
+// console.log(msg[-1]);
+
+
+// console.log(msg.at(2));
+// console.log(msg.at(-1));
+
+// console.log(msg.slice(5));
+// console.log(msg.slice(0,5));
+
+// console.log(msg.split(''));
+// console.log(msg.split('o').join('-'));
+
+
+{
+    let a=10;
+    const b=52;
+    console.log(a);
+}
+
+
+// const b=3;
+{
+let a=12;
+ const b=5;
+
+ console.log(a);
+}
